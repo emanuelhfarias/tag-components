b86e50982df9ddffd37d5e032a6694ab3a0eadfd
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-09-20 18:26:04 -0300

    isolate polyfill function; export module

--- component-polyfill.js
+++ component-polyfill.js
@@ -1,92 +1,94 @@
 "use strict";
 
-function registerComponents () {
-  let components = document.getElementsByTagName("component");
+(() => {
+  function registerComponents () {
+    let components = document.getElementsByTagName("component");
 
-  Array.from(components).forEach((component) => {
-    register(component)
-  })
+    Array.from(components).forEach((component) => {
+      register(component)
+    })
 
-  function register(component) {
-    let componentName = component.getAttribute('name');
+    function register(component) {
+      let componentName = component.getAttribute('name');
 
-    if (!componentName) {
-      console.warning('components must have a name attribute');
-      return;
-    }
-
-    registerTemplate(component);
-    registerRootElement(component);
-    registerStyle(component);
-
-    if (customElements.get(componentName) === undefined) {
-      customElements.define(componentName, baseHtmlElementClass(component));
-    }
-  }
-
-  function registerTemplate(component) {
-    let template = component.getElementsByTagName('template')
-    if (template.length === 0) {
-      component.insertBefore(document.createElement('template'), component.firstChild);
-    }
-  }
-
-  function registerRootElement(component) {
-    let extendsElement = component.getAttribute('extends');
-    let template = component.getElementsByTagName('template');
-    if (template[0].content.firstElementChild === null) {
-      let slot = document.createElement('slot');
-      let rootElement = slot;
-
-      if (extendsElement) {
-        rootElement = document.createElement(extendsElement);
-        rootElement.appendChild(slot);
+      if (!componentName) {
+        console.warning('components must have a name attribute');
+        return;
       }
 
-      template[0].content.appendChild(rootElement);
-    }
-  }
+      registerTemplate(component);
+      registerRootElement(component);
+      registerStyle(component);
 
-  function registerStyle(component) {
-    let style = component.getElementsByTagName('style');
-    if (style.length > 0) {
-      component.getElementsByTagName('template')[0].content.appendChild(style[0]);
+      if (customElements.get(componentName) === undefined) {
+        customElements.define(componentName, baseHtmlElementClass(component));
+      }
     }
-  }
 
-  function baseHtmlElementClass (component) {
-    return class extends HTMLElement {
-      constructor(...args) {
-        const self = super(...args);
-        let template = component.getElementsByTagName('template')[0];
-        if (template) {
-          let templateContent = template.content;
-          const shadowRoot = this.attachShadow({mode: 'open'}).appendChild(templateContent.cloneNode(true));
+    function registerTemplate(component) {
+      let template = component.getElementsByTagName('template')
+      if (template.length === 0) {
+        component.insertBefore(document.createElement('template'), component.firstChild);
+      }
+    }
+
+    function registerRootElement(component) {
+      let extendsElement = component.getAttribute('extends');
+      let template = component.getElementsByTagName('template');
+      if (template[0].content.firstElementChild === null) {
+        let slot = document.createElement('slot');
+        let rootElement = slot;
+
+        if (extendsElement) {
+          rootElement = document.createElement(extendsElement);
+          rootElement.appendChild(slot);
         }
- 
-        //let script = component.getElementsByTagName('script')[0];
-        return self;
+
+        template[0].content.appendChild(rootElement);
       }
+    }
 
-      connectedCallback() {
-        this.propagateAttributes()
+    function registerStyle(component) {
+      let style = component.getElementsByTagName('style');
+      if (style.length > 0) {
+        component.getElementsByTagName('template')[0].content.appendChild(style[0]);
       }
+    }
 
-      propagateAttributes() {
-        let rootElement = this.shadowRoot.firstElementChild
+    function baseHtmlElementClass (component) {
+      return class extends HTMLElement {
+        constructor(...args) {
+          const self = super(...args);
+          let template = component.getElementsByTagName('template')[0];
+          if (template) {
+            let templateContent = template.content;
+            const shadowRoot = this.attachShadow({mode: 'open'}).appendChild(templateContent.cloneNode(true));
+          }
 
-        Array.from(this.attributes).forEach((attribute) => {
-          rootElement.setAttribute(attribute.name, attribute.value);
-        })
+          //let script = component.getElementsByTagName('script')[0];
+          return self;
+        }
+
+        connectedCallback() {
+          this.propagateAttributes()
+        }
+
+        propagateAttributes() {
+          let rootElement = this.shadowRoot.firstElementChild
+
+          Array.from(this.attributes).forEach((attribute) => {
+            rootElement.setAttribute(attribute.name, attribute.value);
+          })
+        }
       }
     }
   }
-}
 
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = registerComponents;
-}
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = registerComponents;
+  }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  registerComponents();
-});
+  document.addEventListener("DOMContentLoaded", function(event) {
+    registerComponents();
+  });
+})()
